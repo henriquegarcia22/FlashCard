9a6146aace0e845b5141510c1889aeecb42db261
henriquegarcia22 <146238839+henriquegarcia22@users.noreply.github.com>
2024-11-12 10:07:38 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,25 @@
 
 criaCartao(
-'Bíblia',
-'Qual é o livro mais curto da Bíblia?',
-'O livro mais curto da Bíblia é Obadias, que possui apenas 21 versículos.'
+'agricultura',
+'O que é agricultura de precisão?',
+'É uma abordagem que utiliza tecnologias avançadas (como GPS e sensores) para monitorar e gerenciar variabilidades nas lavouras, otimizando insumos e aumentando a eficiência.
+'
 )
 
 criaCartao(
-'Bíblia',
+'agricultura',
 'Quem escreveu a maioria dos Salmos?',
 'A maioria dos Salmos é atribuída ao rei Davi, embora outros autores também tenham contribuído.'
 )
 
 criaCartao(
-'Bíblia',
+'agricultura',
 'Qual é o nome do primeiro homem criado segundo a Bíblia?',
 'O primeiro homem criado segundo a Bíblia é Adão.'
 )
 
 criaCartao(
-'Bíblia',
+'agricultura',
 'Qual é o principal ensinamento do Sermão da Montanha?',
 'O Sermão da Montanha, encontrado em Mateus 5-7, ensina sobre a ética do Reino de Deus, incluindo as bem-aventuranças e a importância do amor e da misericórdia.'
 )
