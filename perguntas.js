ece920e630038b766355bf1eb076882dc2b9f0af
henriquegarcia22 <146238839+henriquegarcia22@users.noreply.github.com>
2024-11-26 09:39:10 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,23 @@
-
 criaCartao(
-'agricultura',
-'O que é agricultura de precisão?',
-'É uma abordagem que utiliza tecnologias avançadas (como GPS e sensores) para monitorar e gerenciar variabilidades nas lavouras, otimizando insumos e aumentando a eficiência.',
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-'agricultura',
-'Quais são os principais cultivos do Brasil?',
-'Os principais cultivos incluem soja, milho, cana-de-açúcar, café, arroz e feijão.',
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-'agricultura',
-'O que é agroecologia?',
-'É um sistema que integra práticas agrícolas sustentáveis com princípios ecológicos, promovendo a biodiversidade e a sustentabilidade do solo.',
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-'agricultura',
-'Como a tecnologia está mudando o setor agrícola?',
-'A tecnologia está introduzindo ferramentas como drones, sensores, big data e biotecnologia, melhorando a produtividade, reduzindo custos e permitindo decisões mais informadas.',
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
